b3a4e7fe96110052c8411bf4b3498769528118c5
David <david.joset@yahoo.fr>
2021-11-04 17:28:59 -0400

    Add model, db context and set up startup

---
+++ PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PlateformService.Models;
+
+namespace PlateformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using PlateformService.Models;
+
+namespace PlateformService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform platform);
+    }
+}
