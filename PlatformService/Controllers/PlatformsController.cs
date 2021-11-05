fedb1b41abcc6c4f8df6e0a7b02d164bbced18f6
David <david.joset@yahoo.fr>
2021-11-05 12:35:38 -0400

    Add some http request

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlateformService.Controllers
 {
@@ -26,5 +27,31 @@ namespace PlateformService.Controllers
             
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(_repository.GetAllPlatforms()));
         }
+
+        [HttpGet("{id}", Name = nameof(GetPlatformById))]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+
+            if (platformItem != null)
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatfromCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id}, platformReadDto); 
+        }
+
+
     }
 }
